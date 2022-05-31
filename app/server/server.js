9ead7b59d152cfbd8e0a89c5688e089cd134a042
Anthony <tapiaanthony81@gmail.com>
2022-05-31 17:55:32 -0400

    Added scripts for heroku build

--- app/server/server.js
+++ app/server/server.js
@@ -14,3 +14,13 @@ const PORT = process.env.PORT || 5000;
 db.on("error", console.error.bind(console, "MongoDB Connection Error"));
 
 app.listen(PORT, () => console.log(`Listening on Port ${PORT}`));
+
+// Accessing the path module
+const path = require("path");
+
+// Step 1:
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+// Step 2:
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
